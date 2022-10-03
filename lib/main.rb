a14aca0f0a8a227afa1f2ebecbd921f474bfc013
blpeters <brett.peters@utexas.edu>
2022-10-02 19:15:29 -0500

    turn tracking

--- lib/main.rb
+++ lib/main.rb
@@ -7,25 +7,30 @@ require_relative 'instructions'
 
 # Starts and ends the game, including replays
 class Game
-  attr_accessor :game_over
+  attr_reader :secret_code
+  attr_accessor :game_over, :breaker, :turns
 
   include Instructions
 
   def initialize
     @game_over = false
+    @secret_code = '1234' # Placeholder - will need a codemaker class.
     puts instructions
-    start_game
+    play
   end
 
-  def start_game
-    # Assumes human will play role of breaker for now but
-    # may need to add functionality for computer AI
-    game_logic = Logic.new
+  def play
+    @breaker = Codebreaker.new
+    @turns = 1
+    until game_over
+      get_guess
+      @turns += 1
+    end
   end
 
   def end_game(condition)
     game_over = true
-    condition == 1 ? "you win" : "you lose"
+    condition == 1 ? (puts 'You win!!') : (puts 'You ran out of turns')
     replay
   end
 
@@ -39,42 +44,18 @@ class Game
       exit
     end
   end
-end
-
-# Proceeds through the game logic by handling guesses/results
-# Messages received: What is your answer?
-# Messages sent: This is how correct your answer is
-class Logic
-  attr_reader :secret_code, :breaker
-  attr_accessor :turns, :game_over
-
-  def initialize
-    @game_over = false
-    # The secret code could be obtained from creating a new instance of a
-    # Codemaker class that has a human or computer make a code and send it back
-    @secret_code = '1234'
-    @turns = 0
-    play
-  end
-
-  def play
-    @breaker = Codebreaker.new
-    until game_over
-      get_guess
-    end
-  end
-
+ 
   def get_guess
     check_guess(breaker.guess)
   end
 
   def check_guess(guess_attempt)
-    # turns += 1 #doesn't work
+    p turns
     if code_broken?(guess_attempt)
-      end_game
+      end_game(1)
       true
-    elsif turns == 13
-      end_game
+    elsif turns == 12
+      end_game(0)
       true
     else false
     end
@@ -87,22 +68,21 @@ end
 
 # Codebreaker class - using human input or computer AI, takes guesses at code.
 class Codebreaker
-  attr_reader :turns
-
   def guess
     guess_attempt = ''
     until validate_guess(guess_attempt)
-      print "Enter guess ##{@turns} by entering 4 consecutive numbers(no spaces): "
+      print 'Enter guess by entering 4 consecutive numbers(no spaces): '
       guess_attempt = gets.chomp.to_s
     end
     guess_attempt
   end
 
+  private
+
   def validate_guess(input)
     # regex match for only a 4-digit input with digits between 1 and 6.
     input.match(/^[1-6]{4}$/) ? true : false
   end
-
 end
 
-Game.new
+game = Game.new
